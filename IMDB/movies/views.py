c4b083f8b2cdadfacb56e23729725c1680c551d9
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2022-06-24 13:01:50 +0430

    Bug-fix

--- IMDB/movies/views.py
+++ IMDB/movies/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    # return render(request, 'movies/index.html')
-    return HttpResponse('Hello World!')
+    return render(request, 'movies/index.html', {})
